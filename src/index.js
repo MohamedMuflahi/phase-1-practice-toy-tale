df6f3901bd9cdac6debdc8b1a3f2453479e5b99e
mohamed muflahi <muflahimohamed88@gmail.com>
2022-02-08 15:02:08 -0500

    commit tree b627941afd97dd1ec56673706f46304c944cb688

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+const cardCollection = document.getElementById("toy-collection");
+const nameInput = document.getElementById("nameInput");
+const imageInput = document.getElementById("imageInput");
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener('click', ()=>{
+  addObject();
+});
 let addToy = false;
-
 document.addEventListener("DOMContentLoaded", () => {
+  RenderObjects();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +20,66 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function RenderObjects(){
+  // renders objects from DB
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(data => {
+      //console.log(data[0].name); THIS GETS WOODY
+      data.forEach(element => {
+        let card = document.createElement('div');
+        card.className = "card";
+        card.innerHTML = `
+      <h2>${element.name}</h2>
+      <img src="${element.image}" class="toy-avatar" />
+      <p>${element.likes} Likes</p>
+      <button class="like-btn" id="${element.id}">Like ❤️</button>
+      `;
+      cardCollection.appendChild(card);
+      let button = document.getElementById(element.id);
+      let x = (element.likes +1);
+      button.addEventListener('click',() => {
+        fetch(`http://localhost:3000/toys/${element.id}`, {
+          method: 'PATCH',
+          body: JSON.stringify({
+            likes: x,
+          }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        })
+          .then((response) => response.json())
+          .then((json) => {
+            location.reload();
+          });
+      });
+      })
+      
+      
+  });
+}
+function addObject(){
+  console.log('addObjext called');
+  fetch('http://localhost:3000/toys', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    name: nameInput.value,
+    image: imageInput.value,
+    likes: 0,
+
+  }),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('success');
+  location.reload();
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+}
+
+ // http://localhost:3000/toys
